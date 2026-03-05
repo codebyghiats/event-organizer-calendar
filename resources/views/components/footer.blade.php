649a97767736d74291b0732cc3b8f80161f33d89
yuusosleepy <yudaindramaulida@gmail.com>
2026-03-06 00:28:32 +0700

    update-footer2

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,27 +7,16 @@
 
             <!-- BRAND -->
             <div>
-<<<<<<< HEAD
-                <h2 class="text-2xl font-bold text-white border-b-2 p-1">School Planner</h2>
+                <h2 class="text-2xl font-bold text-white border-b-4 p-2">School Planner</h2>
                 <p class="mt-4 text-slate-400 text-sm leading-relaxed">
-                    <span class="text-white font-semibold ">Platform manajemen agenda sekolah</span> untuk mengatur jadwal
+                    <span class="font-semibold text-white">Platform manajemen agenda sekolah</span> untuk mengatur jadwal
                     OSIS, MPK, Ekstrakurikuler, dan Event Sekolah secara rapi dan profesional.
-=======
-                <h2 class="text-2xl font-bold text-white border-b-2 border-current pb-2">School Planner</h2>
-                <p class="mt-4 text-slate-400 text-sm leading-relaxed">
-                    Platform manajemen agenda sekolah untuk mengatur jadwal
-                    OSIS, MPK, Ekstrakurikuler, dan Event Sekolah secara <span class="font-semibold text-white">rapi dan profesional</span>.
->>>>>>> de4271005ed2398892cdfc8726cf5ce64c29c22a
                 </p>
             </div>
 
             <!-- NAVIGATION -->
             <div>
-<<<<<<< HEAD
-                <h3 class="text-white font-semibold mb-4 border-b-2 p-1">Navigasi</h3>
-=======
-                <h3 class="text-white font-semibold mb-4 border-b-2 border-current pb-2">Navigasi</h3>
->>>>>>> de4271005ed2398892cdfc8726cf5ce64c29c22a
+                <h3 class="text-white font-semibold mb-4 border-b-4 p-2">Navigasi</h3>
                 <ul class="space-y-3 text-sm">
                     <li>
                         <a href="/" class="hover:text-white transition flex items-center gap-2">
@@ -58,11 +47,7 @@
 
             <!-- DEVELOPERS -->
             <div>
-<<<<<<< HEAD
-                <h3 class="text-white font-semibold mb-4 border-b-2 p-1">Tim Developer</h3>
-=======
-                <h3 class="text-white font-semibold mb-4 border-b-2 border-current pb-2">Tim Developer</h3>
->>>>>>> de4271005ed2398892cdfc8726cf5ce64c29c22a
+                <h3 class="text-white font-semibold mb-4 border-b-4 p-2">Tim Developer</h3>
                 <ul class="space-y-3 text-sm">
 
                     <!-- Developer 1 -->
@@ -160,11 +145,7 @@
 
         <!-- BOTTOM -->
         <div class="border-t border-slate-700 mt-12 pt-6 text-center text-sm text-slate-500">
-<<<<<<< HEAD
-           <span class="text-white font-semibold">TR1VIUM</span> © {{ date('Y') }} School Planner | All rights reserved.
-=======
-           TR1VIUM © {{ date('Y') }} School Planner | All rights reserved.
->>>>>>> de4271005ed2398892cdfc8726cf5ce64c29c22a
+           <span class="font-semibold text-white">TR1VIUM</span> © {{ date('Y') }} School Planner |  All rights reserved.
         </div>
     </div>
 </footer>

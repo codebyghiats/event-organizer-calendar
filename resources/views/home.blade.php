f647924c5ab171a8660245df238471cce20a93df
Ghiats Abdurahman Rasyid <ghiatsajalah@gmail.com>
2026-03-05 07:35:41 +0700

    component-js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,38 +29,5 @@
 <x-footer></x-footer>
 
 
-
-
-<script>
-AOS.init({duration:1000,once:true});
-
-/* NAVBAR SCROLL */
-window.addEventListener("scroll",function(){
-const navbar=document.getElementById("navbar");
-if(window.scrollY>50){
-navbar.classList.add("nav-scrolled");
-}else{
-navbar.classList.remove("nav-scrolled");
-}
-});
-
-/* COUNTER */
-const counters=document.querySelectorAll('.counter');
-counters.forEach(counter=>{
-counter.innerText='0';
-const updateCounter=()=>{
-const target=+counter.getAttribute('data-target');
-const c=+counter.innerText;
-const increment=target/100;
-if(c<target){
-counter.innerText=`${Math.ceil(c+increment)}`;
-setTimeout(updateCounter,20);
-}else{
-counter.innerText=target;
-}
-};
-updateCounter();
-});
-</script>
 </body>
 </html>
